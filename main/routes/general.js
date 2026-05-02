63dc5a4b9d14c3b4db7621b7c74f91bc60952dad
vaidyaneham21-oss <vaidyaneham21@gmail.com>
2026-05-02 13:24:48 +0530

    Update general.js

--- main/routes/general.js
+++ main/routes/general.js
@@ -2,28 +2,30 @@ const express = require('express');
 const public_users = express.Router();
 const axios = require('axios');
 
-let books = require("./booksdb.js");
-
-// Get all books using async/await
+// Get all books (async/await)
 public_users.get('/', async function (req, res) {
     try {
         const response = await axios.get('http://localhost:5000/');
         return res.send(response.data);
     } catch (error) {
-        return res.status(500).json({message: "Error fetching books"});
+        return res.status(500).json({ message: "Error fetching all books" });
     }
 });
 
-// Get book by ISBN using promises
+// Get book by ISBN (promise)
 public_users.get('/isbn/:isbn', function (req, res) {
     const isbn = req.params.isbn;
 
     axios.get(`http://localhost:5000/isbn/${isbn}`)
-        .then(response => res.send(response.data))
-        .catch(error => res.status(500).json({message: "Error fetching book"}));
+        .then(response => {
+            return res.send(response.data);
+        })
+        .catch(error => {
+            return res.status(500).json({ message: "Error fetching book by ISBN" });
+        });
 });
 
-// Get books by author using async/await
+// Get books by author (async/await)
 public_users.get('/author/:author', async function (req, res) {
     const author = req.params.author;
 
@@ -31,17 +33,21 @@ public_users.get('/author/:author', async function (req, res) {
         const response = await axios.get(`http://localhost:5000/author/${author}`);
         return res.send(response.data);
     } catch (error) {
-        return res.status(500).json({message: "Error fetching books by author"});
+        return res.status(500).json({ message: "Error fetching books by author" });
     }
 });
 
-// Get books by title using promises
+// Get books by title (promise)
 public_users.get('/title/:title', function (req, res) {
     const title = req.params.title;
 
     axios.get(`http://localhost:5000/title/${title}`)
-        .then(response => res.send(response.data))
-        .catch(error => res.status(500).json({message: "Error fetching books by title"}));
+        .then(response => {
+            return res.send(response.data);
+        })
+        .catch(error => {
+            return res.status(500).json({ message: "Error fetching books by title" });
+        });
 });
 
 module.exports.general = public_users;
